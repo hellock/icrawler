803282d45b6755be3986a1d06bfb1313ef582fbc
Kai Chen <chenkaidev@gmail.com>
2016-04-09 02:21:10 +0800

    modify version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return f.read()
 
 setup(name='icrawler',
-      version='0.1.1',
+      version='0.1.2',
       description='A mini framework of image crawlers',
       long_description='This package is a mini framework for multi-thread image'
                        ' crawler. It also provides some useful built-in'
