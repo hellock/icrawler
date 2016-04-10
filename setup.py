9b6f8927fec207b91a18e262f3dd15007be3bf22
Kai Chen <chenkaidev@gmail.com>
2016-04-10 15:07:34 +0800

    modify version number

--- setup.py
+++ setup.py
@@ -7,12 +7,9 @@ def readme():
         return f.read()
 
 setup(name='icrawler',
-      version='0.1.2',
+      version='0.1.3',
       description='A mini framework of image crawlers',
-      long_description='This package is a mini framework for multi-thread image'
-                       ' crawler. It also provides some useful built-in'
-                       ' crawlers such as google, bing, baidu, flickr image'
-                       ' crawlers and a greedy website crawler.',
+      long_description=readme(),
       keywords='image crawler spider',
       packages=find_packages(),
       classifiers=[
