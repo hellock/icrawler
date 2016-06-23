a1505c3312ab07f4c8845b1eff1ff52ba6868ef3
Lightjohn <johnleger26@gmail.com>
2016-06-23 10:04:30 +0200

    Adding support for images extensions

--- icrawler/downloader.py
+++ icrawler/downloader.py
@@ -51,6 +51,10 @@ class Downloader(object):
         The default strategy is to save images in the img_dir, with an
         increasing 6-digit number as the filename. Users can override this
         method if need to rename the image file or store it in custom paths.
+        
+        The goal is also to preserve the extension of the file.
+        The extension is computed from the url: http://some_url/image.XXX?params=a.
+        Where XXX is the extension.
 
         Args:
             img_task: The task dict got from task_queue.
@@ -58,8 +62,13 @@ class Downloader(object):
         Output:
             Fullpath of the image.
         """
+        old_filename = img_task['img_url'].split("/")[-1]
+        extension_with_params = old_filename.split(".")
+        extension = "jpg"
+        if len(extension_with_params) > 1:
+            extension = extension_with_params[-1].split("?")[0]
         filename = os.path.join(self.img_dir,
-                                '{:0>6d}.jpg'.format(self.fetched_num))
+                                ('{:0>6d}.'+extension).format(self.fetched_num))
         return filename
 
     def reach_max_num(self):
