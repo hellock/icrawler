128a65ce29f5365eb1aa4df1abb142f3044737cd
Kai Chen <chenkaidev@gmail.com>
2017-07-31 10:43:05 +0800

    move the dependency check to the init of GoogleStorage

--- icrawler/storage/__init__.py
+++ icrawler/storage/__init__.py
@@ -1,10 +1,5 @@
 from .base import BaseStorage
 from .filesystem import FileSystem
-try:
-    from .google_storage import GoogleStorage
-except ImportError as e:
-    print('Google Cloud python API Missing')
-    __all__ = ['BaseStorage','FileSystem']
-    pass
+from .google_storage import GoogleStorage
 
 __all__ = ['BaseStorage', 'FileSystem', 'GoogleStorage']
