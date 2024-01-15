9151517a1332a5b3b7c3dde516479da734ac1055
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2024-01-15 18:49:23 -0500

    Add files via upload

--- examples/iCrawlerTK.py
+++ examples/iCrawlerTK.py
@@ -16,13 +16,15 @@ import re
 
 # for non-frame app
 class MainApplication:
+
+
     def __init__(self, master, *args, **kwargs):
         self.master = master
         self.frame = tk.Frame(master)
         # master.title("iCrawler")
         # master.geometry("{}x{}".format(460, 350))
         
-        padding_size=7
+        padding_size=10
         
         root.grid_rowconfigure(2, weight=1)
         root.grid_columnconfigure(1, weight=1)
@@ -34,60 +36,46 @@ class MainApplication:
         label_search_string.grid(row=1, column=0, padx=padding_size, pady=padding_size)
         
         # TODO: use a Text box and search each line as a keyword?
-        entry_search_string = tk.Entry(master)
+        self.search_string =tk.StringVar(root)
+        entry_search_string = tk.Entry(master, textvariable=self.search_string)
         entry_search_string.grid(row=1, column=1, padx=padding_size, pady=padding_size)
-
         label_crawlers = tk.Label(master, text="Crawlers: ")
         label_crawlers.grid(row=2, column=0, padx=padding_size, pady=padding_size)
         
         crawlers_frame = tk.Frame(master)
         crawlers_frame.grid(row=2, column=1, padx=padding_size, pady=padding_size)
         
-        crawlers_google = tk.BooleanVar()
-        tk.Checkbutton(crawlers_frame, text="Google", variable=crawlers_google).pack(anchor=tk.W)
-        crawlers_bing = tk.BooleanVar()
-        tk.Checkbutton(crawlers_frame, text="Bing", variable=crawlers_bing).pack(anchor=tk.W)
-        crawlers_baidu = tk.BooleanVar()
-        tk.Checkbutton(crawlers_frame, text="Baidu", variable=crawlers_baidu).pack(anchor=tk.W)
+        self.crawlers_google = tk.BooleanVar()
+        tk.Checkbutton(crawlers_frame, text="Google", variable=self.crawlers_google).pack(anchor=tk.W)
+        self.crawlers_bing = tk.BooleanVar()
+        tk.Checkbutton(crawlers_frame, text="Bing", variable=self.crawlers_bing).pack(anchor=tk.W)
+        self.crawlers_baidu = tk.BooleanVar()
+        tk.Checkbutton(crawlers_frame, text="Baidu", variable=self.crawlers_baidu).pack(anchor=tk.W)
         
 
-        crawl_button = tk.Button(master, text="Go")
+        crawl_button = tk.Button(master, command=self.go_clicked, text="Go")
         crawl_button.grid(row=3, columnspan=2, padx=padding_size, pady=padding_size)
 
-# for frame app
-# class MainApplication(tk.Frame):
-    # def __init__(self, parent, *args, **kwargs):
-        # self.frame = tk.Frame.__init__(self, parent, *args, **kwargs)
-        # self.parent = parent
-        # parent.title("iCrawler")
-        # parent.geometry("{}x{}".format(460, 350))
-        
-        # greeting = tk.Label(self, text="iCrawler Options")
-        # greeting.pack()
+    def go_clicked(self):
 
-        # label_search_string = tk.Label(self, text="Search For: ")
-        # label_search_string.pack()
-        
-        # # TODO: use a Text box and search each line as a keyword?
-        # entry_search_string = tk.Entry(self)
-        # entry_search_string.pack()
+        max_number=1000
+        threads=10
 
-        # label_crawlers = tk.Label(self, text="Crawlers: ")
-        # label_crawlers.pack()
+        search_string   =self.search_string.get()
+        crawlers_bing   =self.crawlers_bing.get()
+        crawlers_baidu  =self.crawlers_baidu.get()
+        crawlers_google =self.crawlers_google.get()
 
-def start_download(max_number, search_string, threads):
+        start_download(crawlers_bing, crawlers_baidu, crawlers_google, search_string, max_number, threads)
 
-    # get from UI
-    test_bing = True
-    test_baidu = True
-    test_google = True
-    test_greedy = False
-    test_urllist = False
-    search_string = "TODO"
+
+def start_download(crawlers_bing, crawlers_baidu, crawlers_google, search_string, max_number, threads):
+
+    search_string = search_string.replace(" ", "_")
     search_folder_name = re.sub('[^a-zA-Z0-9_]', '', search_string)
 
 
-    if test_google:
+    if crawlers_google:
         print("start testing GoogleImageCrawler")
         google_crawler = GoogleImageCrawler(
             downloader_threads=threads, storage={"root_dir": search_folder_name + "/google"}, log_level=logging.INFO
@@ -98,7 +86,7 @@ def start_download(max_number, search_string, threads):
         google_crawler.crawl(search_string, filters=search_filters, max_num=max_number)
 
 
-    if  test_bing:
+    if  crawlers_bing:
         print("start testing BingImageCrawler")
         bing_crawler = BingImageCrawler(downloader_threads=threads, storage={"root_dir": search_folder_name + "/bing"}, log_level=logging.INFO)
     #   search_filters = dict(type="photo", license="commercial", layout="wide", size="large", date="pastmonth")
@@ -106,24 +94,11 @@ def start_download(max_number, search_string, threads):
         bing_crawler.crawl(search_string, filters = search_filters, max_num=max_number)
 
 
-    if test_baidu:
+    if crawlers_baidu:
         print("start testing BaiduImageCrawler")
         search_filters = dict(size="large", color="blue")
         baidu_crawler = BaiduImageCrawler(downloader_threads=threads, storage={"root_dir": search_folder_name + "/baidu"})
         baidu_crawler.crawl(search_string, max_num=max_number)
-
-
-    if test_greedy:
-        print("start testing GreedyImageCrawler")
-        greedy_crawler = GreedyImageCrawler(parser_threads=4, storage={"root_dir": search_folder_name + "/greedy"})
-        greedy_crawler.crawl("http://www.bbc.com/news", max_num=10, min_size=(100, 100))
-
-
-    if test_urllist:
-        print("start testing UrlListCrawler")
-        urllist_crawler = UrlListCrawler(downloader_threads=3, storage={"root_dir": search_folder_name + "/urllist"})
-        filelist = osp.join(osp.dirname(__file__), "filelist_demo.txt")
-        urllist_crawler.crawl(filelist)
 
 
 def main():
