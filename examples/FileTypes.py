2248777a02dc00031ebdffafb576563172287700
Patty-OFurniture <127074553+Patty-OFurniture@users.noreply.github.com>
2024-01-29 18:15:37 -0500

    filetype detector sorta like imghdr.what

---
+++ examples/FileTypes.py
@@ -0,0 +1,48 @@
+# imghdr.what() does not test correctly,
+# these tests were added for another project,
+# but it makes sense to ignore that library 
+# since we have additions and deletions and
+# fixes, just do it here
+
+def is_media(header=None, extension=None, extensions = None):
+    if extension is None and header is None:
+        print("TODO: error")
+    if extensions is None:
+        extensions = ["jpg", "png", "gif", "webp"]
+    if extension is None:
+        extension = get_filetype(header)
+    if extension in extensions:
+        return True
+    return False
+
+def get_filetype(h):
+    if (h[:3] == "\xff\xd8\xff"):
+        return "jpg"
+    if h[6:10] in (b'JFIF', b'Exif'):
+        return 'jpg' # "jpeg"
+
+    if h.startswith(b'\211PNG\r\n\032\n'):
+        return 'png'
+
+    if h[:6] in (b'GIF87a', b'GIF89a'):
+        return 'gif'
+
+    if h[:2] in (b'MM', b'II'):
+        return 'tiff'
+
+    if h.startswith(b'RIFF') and h[8:12] == b'WEBP':
+        return 'webp'
+
+    if b"<html" in h:
+        return "html"
+    if b"<HTML" in h:
+        return "html"
+    if b"<!DOCTYPE " in h: # "<!DOCTYPE HTML" or "<!DOCTYPE html"
+        return "html"
+    if b"<!doctype html" in h:
+        return "html"
+
+    if b"<xml" in h:
+        return "xml"
+    if b"<?xml " in h:
+        return "xml"
--- examples/FilenameDownloader.py
+++ examples/FilenameDownloader.py
@@ -1,7 +1,19 @@
 # based on example https://icrawler.readthedocs.io/en/latest/extend.html
+# 
+# set in Downloader::download()
+# task["file_url"] = ""
+# task["filename"] = Downloader::get_filename()
+# task["success"] = True
+# 
+# task["img_size"] = img.size
+
 from icrawler import ImageDownloader
 from six.moves.urllib.parse import urlparse
+from os import path
 import re
+import logging
+
+import FileTypes
 
 # https://stackoverflow.com/questions/1167617/in-python-how-do-i-indicate-im-overriding-a-method
 def overrides(interface_class):
@@ -13,15 +25,63 @@ def overrides(interface_class):
 class FilenameDownloader(ImageDownloader):
 
     @overrides(ImageDownloader)
+    def keep_file(self, task, response, min_size=None, max_size=None):
+
+        if not super().keep_file(task, response, min_size, max_size):
+            return False
+
+        h = response.content[:32]
+        extension = FileTypes.get_filetype(h)
+
+        self.logger.warn(response.headers)
+
+        #  save filename from Content-Disposition if possible, otherwise from get_filename()
+        disposition_magic = "Content-Disposition"
+        if disposition_magic in response.headers:
+            disposition = response.headers[disposition_magic]
+            self.logger.warn("disposition: {}".format(disposition))
+            filename = disposition.split("filename=")[-1]
+            # ex: "disposition: inline; filename="Kentucky2015.jpg"; filename*=UTF-8''Kentucky2015.jpg"
+            filename = filename.split(";")[0]
+            if filename[0] == '"' and filename[-1] == '"':
+                filename = filename[1:len(filename)-1]
+        else:
+            filename = self.get_filename(task, extension)
+
+        self.logger.warn("filename: {}".format(filename))
+
+        (filename, ext) = path.splitext(filename)
+
+        task["filename"] = filename
+        task["filetype"] = extension
+
+        # TODO: mime sniffing here if needed "Content-Type":
+        # attempt to revert downloader.py
+
+        return True
+
+    @overrides(ImageDownloader)
     def get_filename(self, task, default_ext):
+        max_length = 240
+        if task["filename"]:
+            if task["filetype"]:
+                filename = task["filename"][:max_length] + "." + task["filetype"]
+            else:
+                filename = task["filename"][:max_length]
+        else:
+            url_path = urlparse(task["file_url"]).path
+            filename = url_path.split("/")[-1]
 
-        url_path = urlparse(task["file_url"]).path
-        file_name = url_path.split("/")[-1]
+            if "." not in filename and default_ext:
+                filename = filename[:max_length-1-len(default_ext)] + "." + default_ext
 
-        if "." not in url_path:
-            file_name = file_name + "." + default_ext
+            filename = filename.replace(" ", "_")
+            filename = re.sub('[^a-zA-Z0-9_.]', '', filename)
 
-        file_name = file_name.replace(" ", "_")
-        file_name = re.sub('[^a-zA-Z0-9_.]', '', file_name)
+            if len(filename) > max_length:
+                (filename, ext) = path.splitext(filename)
+                filename = filename[:max_length-1-len(ext)] + "." + ext
 
-        return file_name
+        self.logger.warn("get_filename: {}".format(filename))
+
+        return filename
