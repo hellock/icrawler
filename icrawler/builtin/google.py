15905dfc8b187ed45776f42d7f2de71aa5bc53c0
nikita <wot_98_98@mail.ru>
2024-04-04 10:17:22 +0600

    change parse function

--- icrawler/builtin/google.py
+++ icrawler/builtin/google.py
@@ -141,26 +141,11 @@ class GoogleFeeder(Feeder):
             self.logger.debug(f"put url to url_queue: {url}")
 
 
-class GoogleParser(Parser):
     def parse(self, response):
         soup = BeautifulSoup(response.content.decode("utf-8", "ignore"), "lxml")
-        # image_divs = soup.find_all('script')
-        image_divs = soup.find_all(name="script")
-        for div in image_divs:
-            # txt = div.text
-            txt = str(div)
-            # if not txt.startswith('AF_initDataCallback'):
-            if "AF_initDataCallback" not in txt:
-                continue
-            if "ds:0" in txt or "ds:1" not in txt:
-                continue
-            # txt = re.sub(r"^AF_initDataCallback\({.*key: 'ds:(\d)'.+data:function\(\){return (.+)}}\);?$",
-            #             "\\2", txt, 0, re.DOTALL)
-            # meta = json.loads(txt)
-            # data = meta[31][0][12][2]
-            # uris = [img[1][3][0] for img in data if img[0] == 1]
-
-            uris = re.findall(r"http[^\[]*?\.(?:jpg|png|bmp)", txt)
+        uris = [uri for div in soup.find_all(name="script") for uri in re.findall(r"http[^\[]*?\.(?:jpg|png|bmp)", str(div))]
+        uris = [bytes(uri, 'utf-8').decode('unicode-escape') for uri in uris]
+        if uris:
             return [{"file_url": uri} for uri in uris]
 
 
