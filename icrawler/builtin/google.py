e062fd02c0a0d5defcd7b757a953f088228d4adf
Zhiyuan Chen <this@zyc.ai>
2024-05-15 12:00:16 +0800

    fix GoogleCrawler
    fixes #96, #107, #117, #125

--- icrawler/builtin/google.py
+++ icrawler/builtin/google.py
@@ -150,15 +150,20 @@ class GoogleParser(Parser):
             # txt = div.text
             txt = str(div)
             # if not txt.startswith('AF_initDataCallback'):
-            if "AF_initDataCallback" not in txt:
-                continue
-            if "ds:0" in txt or "ds:1" not in txt:
-                continue
+            # if "AF_initDataCallback" not in txt:
+            #     continue
+            # if "ds:0" in txt or "ds:1" not in txt:
+            #     continue
             # txt = re.sub(r"^AF_initDataCallback\({.*key: 'ds:(\d)'.+data:function\(\){return (.+)}}\);?$",
             #             "\\2", txt, 0, re.DOTALL)
             # meta = json.loads(txt)
             # data = meta[31][0][12][2]
             # uris = [img[1][3][0] for img in data if img[0] == 1]
+
+            uris = re.findall(r"http[^\[]*?.(?:jpg|png|bmp)", txt)
+            uris = [bytes(uri, "utf-8").decode("unicode-escape") for uri in uris]
+            if uris:
+                return [{"file_url": uri} for uri in uris]
 
             uris = re.findall(r"http[^\[]*?\.(?:jpg|png|bmp)", txt)
             return [{"file_url": uri} for uri in uris]
